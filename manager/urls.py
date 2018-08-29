eeb5583c8e7bd88fd0ae31b7af4bea5f1cc9c338
BarAzulay <41300710+BarAzulay@users.noreply.github.com>
2018-08-29 13:01:36 +0300

    Add files via upload

---
+++ manager/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url, include
+from . import views
+
+app_name = 'manager'
+
+
+
+
+
+
+urlpatterns = [
+    url(r"menu/$", views.manager_control, name="manager"),
+    url(r"workout/(?P<username>[-\w]+)/$", views.work_list, name="work_list"),
+    url(r"plans(?P<username>[-\w]+)/$", views.plan_list, name="list"),
+]
+
+
